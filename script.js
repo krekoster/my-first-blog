7b914031902a61709f12d70139359345932528bd
krekoster <72513456+krekoster@users.noreply.github.com>
2023-09-12 19:29:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function toggleLinksDisplay() {
+    let menu = document.getElementById("myLinks");
+    let content = document.getElementById("content");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+        content.style.paddingTop = "5em";
+    }
+    else {
+        menu.style.display = "block";
+        content.style.paddingTop = "14em";
+    }
+}
+
+$(document).ready(function(){
+    $(".preview_container").click(function(){
+        if ($("#holder").is(":visible")) {
+            $("#holder").empty();
+            $("#holder").hide();
+            $(".slike_druge").hide();
+        }
+        else { 
+            $(".slike_druge").show();
+            $("#holder").show();  
+            $(this).clone().appendTo("#holder");
+            // $("#holder").focus("#holder");
+            $(window).scrollTop(0);
+            console.log("fokus"); 
+        }  
+    });
+    $("#holder").click(function() {
+        $("#holder").empty();
+        $("#holder").hide();
+        $(".slike_druge").hide();
+    });
+});
